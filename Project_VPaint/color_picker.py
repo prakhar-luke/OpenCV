fa76ebe13815fc2922faab1e4975ff6583f2581b
Prakhar <prakhar.luke@gmail.com>
2023-09-20 17:52:13 +0530

    Added virtual paint project

---
+++ Project_VPaint/color_picker.py
@@ -0,0 +1,72 @@
+"""
+Use this to pick HSV value for your color pen
+then hard code it in main.py
+"""
+
+import cv2
+import numpy as np
+
+
+def empty(a):
+    pass
+
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range(0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]),
+                                                None, scale, scale)
+                if len(imgArray[x][y].shape) == 2:imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
+
+
+cv2.namedWindow("TrackBars")
+cv2.resizeWindow("TrackBars", 640, 240)
+cv2.createTrackbar("Hue Min", "TrackBars", 2, 179, empty)
+cv2.createTrackbar("Hue Max", "TrackBars", 179, 179, empty)
+cv2.createTrackbar("Sat Min", "TrackBars", 65, 255, empty)
+cv2.createTrackbar("Sat Max", "TrackBars", 255, 255, empty)
+cv2.createTrackbar("Val Min", "TrackBars", 199, 255, empty)
+cv2.createTrackbar("Val Max", "TrackBars", 255, 255, empty)
+
+cap = cv2.VideoCapture(0)
+while True:
+    sucess, img = cap.read()
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h_min = cv2.getTrackbarPos("Hue Min", "TrackBars")
+    h_max = cv2.getTrackbarPos("Hue Max", "TrackBars")
+    s_min = cv2.getTrackbarPos("Sat Min", "TrackBars")
+    s_max = cv2.getTrackbarPos("Sat Max", "TrackBars")
+    v_min = cv2.getTrackbarPos("Val Min", "TrackBars")
+    v_max = cv2.getTrackbarPos("Val Max", "TrackBars")
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max])
+    mask = cv2.inRange(img_hsv, lower, upper)
+    result_img = cv2.bitwise_and(img, img, mask=mask)
+    imgStack = stackImages(0.6, ([img, img_hsv], [mask, result_img]))
+    cv2.imshow("Stacked Images", imgStack)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
---
+++ Project_VPaint/main.py
@@ -0,0 +1,63 @@
+import cv2
+import numpy as np
+
+
+frame_h = 640
+frame_w = 480
+cap = cv2.VideoCapture(0)
+cap.set(3, frame_w)
+cap.set(4, frame_h)
+cap.set(10, 100) # brightness
+
+pen_colors = [[0,9,207,255,121,255],
+                  [68,105,128,255,0,255],
+                  [56,179,196,255,106,107]]
+color_values = [[0,0,255],
+                [255,0,0],
+                [0,128,0]]
+points = []  # [x, y, color_idx]
+def find_color(img, pen_colors, color_values):
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    count = 0
+    new_point = []
+    for colours in pen_colors:
+        lower = np.array(colours[0:3])
+        upper = np.array(colours[3:6])
+        mask = cv2.inRange(img_hsv, lower, upper)
+        x, y = getContours(mask)
+        cv2.circle(result_img, (x,y), 10, color_values[count], cv2.FILLED)
+        if x!= 0 and y != 0:
+            new_point.append([x,y,count])
+        count += 1
+    return new_point
+
+def getContours(img):
+    countours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    x, y, w, h = 0, 0, 0, 0
+    for i in countours:
+        area = cv2.contourArea(i)
+        if area > 100:
+            cv2.drawContours(result_img, i, -1, (255,0,0), 3)
+            perimeter = cv2.arcLength(i, True)
+            aprox_corner_point = cv2.approxPolyDP(i, 0.02*perimeter, True)
+            obj_corner = len(aprox_corner_point)
+            x, y, w, h = cv2.boundingRect(aprox_corner_point)
+    return x+w//2, y
+
+def draw_lines(points, color_values):
+    for i in points:
+        cv2.circle(result_img, (i[0],i[1]), 10, color_values[i[2]], cv2.FILLED)
+
+while True:
+    success , frame = cap.read()
+    result_img = frame.copy()
+    new_point = find_color(frame, pen_colors, color_values)
+    if len(new_point) != 0:
+        for i in new_point:
+            points.append(i)
+    if len(points) != 0:
+        draw_lines(points, color_values)
+    cv2.imshow('Frame', frame)
+    cv2.imshow('result_img', result_img)
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
